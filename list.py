fe0109cbbaecd7e262711a00403d06ea75f49e63
prio101 <miprio101@gmail.com>
2021-03-05 15:17:33 +0600

    added the list

--- list.py
+++ list.py
@@ -2,33 +2,33 @@ if __name__ == '__main__':
     N = int(raw_input())
     
     inputs =[]
-    
+    result = []
+
     for input in range(0, N):
       input = raw_input()
       inputs.append(input.split())
-
-    print(inputs)
     
     result_list = []
 
     for item in inputs:
       temp_list = []
       action_name = item[0]
-      temp_list = item - item[0]
+      temp_list = item.pop(0)
 
       if(action_name == 'insert'):
-        result_list.insert(temp_list)
+        result_list.insert(int(item[0]), int(item[1]))
       elif(action_name == 'remove'):
-        result_list.remove(temp_list)
+        result_list.remove(int(item[0]))
       elif(action_name == 'append'):
-        result_list.append(temp_list)
+        result_list.append(int(item[0]))
       elif(action_name == 'pop'):
-        result_list.pop(temp_list)
+        if(len(result_list)>=1):
+          result_list.pop()
       elif(action_name == 'reverse'):
         result_list.reverse()
       elif(action_name == 'sort'):
         result_list.sort()
       elif(action_name == 'print'):
         print(result_list)
-      else
-        return None
+      else:
+       None
